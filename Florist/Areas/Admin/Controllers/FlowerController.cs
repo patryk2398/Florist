8c50168abf359462f3d166ba42ad5184653d9353
Patryk Jaworski <patryk.jaworski23@gmail.com>
2020-07-06 14:42:15 +0200

    add index method in flower controller

---
+++ Florist/Areas/Admin/Controllers/FlowerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Florist.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Florist.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FlowerController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FlowerController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
+        {
+            _db = db;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Flower = await _db.Flower.ToListAsync();
+            return View(Flower);
+        }
+    }
+}
