3c5a3ba1e96959cceb0cf1ba7f46769c4fc6a2c6
Patryk Jaworski <patryk.jaworski23@gmail.com>
2020-09-22 12:10:25 +0200

    Revert "Frontend changes"
    This reverts commit 49f78847a7b924b4d8ca1e578a8e89e39f90e8a3.

--- Florist/Data/DbInitializer.cs
+++ Florist/Data/DbInitializer.cs
@@ -47,7 +47,7 @@ namespace Florist.Data
                 Email = "admin@gmail.com",
                 FirstName = "Patryk",
                 LastName = "Jaworski"
-            }, "zaq1@WSX").GetAwaiter().GetResult();
+            }, "Admin123*").GetAwaiter().GetResult();
 
             IdentityUser user = await _db.Users.FirstOrDefaultAsync(u => u.Email == "admin@gmail.com");
 
