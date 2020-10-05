bf73d80fcece7ba37d48d0c9aa25f9a1cdc4ac71
Patryk Jaworski <patryk.jaworski23@gmail.com>
2020-10-05 18:52:12 +0200

    Add Contact page

--- Florist/Areas/Customer/Controllers/HomeController.cs
+++ Florist/Areas/Customer/Controllers/HomeController.cs
@@ -47,6 +47,11 @@ namespace Florist.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
